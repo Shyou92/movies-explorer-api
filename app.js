14e44e0b68aef4f8aa144ef2e3308a445f698c24
Alexander Vitchinov <vitalex92@yandex.ru>
2021-03-11 19:28:55 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+})
+
+app.listen(PORT, () => {
+  console.log(`Application is being listened on port ${PORT}`);
+})
