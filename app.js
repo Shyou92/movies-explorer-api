716c888c50bd50c3d641e477a69d960a24bcac67
Alexander Vitchinov <vitalex92@yandex.ru>
2021-03-17 23:03:57 +0300

    env1

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const {
-  APP_PORT = 4000,
+  APP_PORT = '4000',
   DB_HOST = 'localhost',
-  DB_PORT = 27017,
+  DB_PORT = '27017',
   DB_NAME = 'bitfilmsdb',
 } = require('./config');
 const routes = require('./routes');
